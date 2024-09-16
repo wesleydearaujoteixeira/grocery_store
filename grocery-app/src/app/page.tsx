c5b1e72a6a5606c4ed6d0c610c05ab6cab958d35
Wesley <wesleyaraujoteixeira98@gmail.com>
2024-09-16 10:26:33 -0300

    projeto iniciado

---
+++ grocery-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1> botão </h1>
+      <Button> Subscribe </Button>
+    </div>
+  );
+}
